74a1245fae1078dc3d6d7573591e6c6bde668ca4
SmartBug <samaku94@gmail.com>
2025-11-25 22:18:52 +0100

    added some fuctions

--- main.go
+++ main.go
@@ -4,15 +4,16 @@ import "sync"
 
 // in Bayou we just have writes
 // here we have the base for a Write to identify its herkunft und timestamp
-type writeID struct {
-	CSN            int64
+// if CSN == -1 then we dont have any commit of this write yet, or global order
+type writeStamp struct {
+	commitNum      int64
 	localTimestamp int64
 	serverID       int64
 }
 
 // das ist die reprawesentation von einem eigentlichen write mit seiner ID
 type write struct {
-	ID      writeID
+	ID      writeStamp
 	payload string
 }
 
@@ -23,8 +24,29 @@ type logEntry struct {
 }
 
 // this would represent a server
+// we need a lamportclock implementation to use in a server for the timestamp in the writeStamp
+// TODO implement a lamport clock to use in each server
 type server struct {
 	ID         int64
 	log        []logEntry
 	serverLock sync.Mutex
+	//serverClock laportClock
+}
+
+func newWrite(serv server, op string) write {
+	return write{
+		ID: writeStamp{
+			commitNum:      -1,
+			localTimestamp: 0, // TODO replace with lamport clock of server
+			serverID:       serv.ID,
+		},
+		payload: op,
+	}
+}
+
+func newServer(id int64) *server {
+	return &server{
+		ID:  id,
+		log: make([]logEntry, 0),
+	}
 }
