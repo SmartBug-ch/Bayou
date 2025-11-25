6c54eda9bdbefe751bd698ead8dac05d0c4e5a24
SmartBug <samaku94@gmail.com>
2025-11-25 18:15:29 +0100

    added some base Structs 2

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-// in Bayou we just have writes here ist the ground structure of a write
-type write struct {
+import "sync"
+
+// in Bayou we just have writes
+// here we have the base for a Write to identify its herkunft und timestamp
+type writeID struct {
 	CSN            int64
 	localTimestamp int64
 	serverID       int64
+}
+
+// das ist die reprawesentation von einem eigentlichen write mit seiner ID
+type write struct {
+	ID      writeID
+	payload string
 }
 
 // we need a logentry type to create the logs itself in the server
@@ -15,10 +24,7 @@ type logEntry struct {
 
 // this would represent a server
 type server struct {
-	ID  int64
-	log []logEntry
-}
-
-func main() {
-
+	ID         int64
+	log        []logEntry
+	serverLock sync.Mutex
 }
